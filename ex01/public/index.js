e690c162ad0b0e95be1e69a072b5770b052fffb6
shin-dongheon <tlsejdgjs@naver.com>
2023-10-27 15:58:26 +0900

    #

--- ex01/public/index.js
+++
@@ -1,8 +0,0 @@
-function App(){
-    const App =  document.createElement('div');
-    App.textContent = " HI HI HELLO ";
-    return App;
-}
-
-document
-    .getElementById('root').appendChild(App());
