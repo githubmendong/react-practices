07a69ad99f3f4ef8d7629b258fcdef4fcb9e6be0
shin-dongheon <tlsejdgjs@naver.com>
2023-10-30 16:46:37 +0900

    #

---
+++ 1.basics/ex05/src/App.js
@@ -0,0 +1,23 @@
+// import './assets/scss/App.scss';
+// import logo from './assets/images/logo.svg';
+import React from 'react';
+
+function App() {
+    const App = React.createElement('div',null,'Helloworld');
+    return App;
+    //
+    // return (
+    //     <div>
+    //         hello wrold
+    //         <Ul>
+    //             {
+    //                 ['1st', '2nd'].map((e) => {
+    //                     return <li>${e}</li>
+    //                 })
+    //             }
+    //         </Ul>
+    //     </div>
+    // )
+}
+
+export {App};
---
+++ 9.webpack-practice/ex04/src/App.js
@@ -0,0 +1,9 @@
+import './assets/css/App.css'
+
+function App() {
+    const App = document.createElement('div');
+    App.textContent = "Hello Webpack";
+    return App;
+}
+
+export {App};
