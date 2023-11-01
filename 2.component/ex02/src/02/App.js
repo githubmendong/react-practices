e6c90d038618c1dabf850a7892c0243aa74e9d1e
shin-dongheon <tlsejdgjs@naver.com>
2023-11-01 13:19:18 +0900

    #

---
+++ 2.component/ex02/src/02/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div id={'App'}>
+            <h12 style={{
+
+            }}>inline styling
+            </h12>
+        </div>
+    );
+}
+
+export {App};
