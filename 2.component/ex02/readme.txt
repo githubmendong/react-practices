c2e10dba19a35f2f53dacf78f4773072a19176e8
shin-dongheon <tlsejdgjs@naver.com>
2023-11-02 12:42:09 +0900

    # Event -2

--- 2.component/ex02/readme.txt
+++ 2.component/ex02/readme.txt
@@ -1,14 +1,32 @@
-ex01: property
-1.  컴포넌트의 데이터
-2.  부모에서 자식으로 전달된다.
-    - 컴포넌트 통신(Data Flow)
-    - Top -> Down
-3.  자식에서 변경 불가
-4.  부모가 소유한 데이터 
+ex02: Component Styling
+1) inline styling
+2) Normal CSS
+   - css-loader option: {module: false}
+   - 실행
+     $ npm run debug src=02 css-module=false
 
-src/01: 함수 컴포넌트
-src:02: Data Flow
+3) CSS Module I
+   - css-loader option: {module: true}
+   - 실행
+     $ npm run debug src=03
 
+4) CSS Module II
+   - css-loader option: {module: true}
+   - 실행
+     $ npm run debug src=04
+
+5) SASS & SCSS
+   - css-loader option: {module: true}
+   - 실행
+     $ npm run debug src=05
+
+6) CSS in JS: Styled Component
+    - $ npm i style-components
+    - css-loader option:{module:true}
+    - 실행
+        $npm run debug src = 06
+7) React Modal
+8) Modal: Material UI
 
 ====================================================================================
 
@@ -16,7 +34,7 @@ src:02: Data Flow
     1)  개발툴
         $ npm i -D webpack webpack-cli webpack-dev-server css-loader style-loader sass-loader node-sass babel-loader @babel/core @babel/preset-env @babel/preset-react
     2)  라이브러리
-        $ npm i react react-dom
+        $ npm i react react-dom prop-types
 
 2.  설정
     1)  webpack.config.js
--- kanbanboard/frontend/src/Kanbanboard.js
+++ kanbanboard/frontend/src/Kanbanboard.js
@@ -2,16 +2,43 @@ import React from 'react';
 import data from './assets/json/data';
 
 function Kanbanboard(props) {
-
     const cardsToDo = data.filter(card => card.status === 'ToDo');
-    console.log(cardsToDo);
+    const cardsDoing = data.filter(card => card.status === 'Doing');
+    const cardsDone = data.filter(card => card.status === 'Done');
 
     return (
         <div>
-            Kanbanboard
+            <h1>Kanbanboard</h1>
+            <div>
+                <h2>To Do</h2>
+                {cardsToDo.map((card, index) => (
+                    <div key={index}>
+                        <h3>{card.title}</h3>
+                        <p>{card.description}</p>
+                    </div>
+                ))}
+            </div>
+
+            <div>
+                <h2>Doing</h2>
+                {cardsDoing.map((card, index) => (
+                    <div key={index}>
+                        <h3>{card.title}</h3>
+                        <p>{card.description}</p>
+                    </div>
+                ))}
+            </div>
+
+            <div>
+                <h2>Done</h2>
+                {cardsDone.map((card, index) => (
+                    <div key={index}>
+                        <h3>{card.title}</h3>
+                        <p>{card.description}</p>
+                    </div>
+                ))}
+            </div>
         </div>
-            
-        
     );
 }
 
